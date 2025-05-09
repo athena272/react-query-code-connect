227f7bfd7062047e41093895803049fab9718625
athena272 <guilhermera272@gmail.com>
2025-05-08 22:31:49 -0300

    Delete index.jsx

--- src/components/Button/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./button.module.css";
-
-export const Button = ({ children, disable }) => {
-  return (
-    <button className={styles.btn} disabled={disable}>
-      {children}
-    </button>
-  );
-};
